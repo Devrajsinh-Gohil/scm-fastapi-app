347c1231f42d7df3a5c81d1a55b46c1b0e48a34f
Devrajsinh Gohil <devrajsinhgohil@192.168.1.3>
2025-06-24 00:04:48 +0530

    fix: use dynamic port from Render workers to 1

--- main.py
+++ main.py
@@ -74,4 +74,4 @@ async def reply(request: Request):
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, workers=5)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, workers=1)
